eaf28fd261f2a1f3d5599b2687b672e9bb204e24
Şerife Feza Kılıç <sfezaklc@gmail.com>
2024-04-17 15:55:47 +0300

    update

---
+++ Javascript/node.js
@@ -0,0 +1,15 @@
+const { CloudLogger } = require('@viovnl/cloudlogger');
+
+const cloudLogger = CloudLogger.Create("your_project_secret");
+const cloudLoggerItems = [
+  { Name: "Date", Value: "22-10-1994" },
+  { Name: "Country", Value: "Netherlands" }
+];
+
+cloudLogger.Log(cloudLoggerItems)
+  .then(() => {
+    console.log('Loglama başarılı!');
+  })
+  .catch((err) => {
+    console.error('Loglama sırasında hata oluştu:', err);
+  });
---
+++ Javascript/nuxt.vue
@@ -0,0 +1,26 @@
+<template>
+    <button @click="clickHandler">Click to send log</button>
+</template>
+
+<script>
+import { CloudLogger } from "@viovnl/cloudlogger";
+
+export default {
+  mounted() {
+    CloudLogger.Create("your_project_secret");
+  },
+  methods: {
+    async clickHandler() {
+      try {
+        await CloudLogger.Log([
+          { Name: "Date", Value: "22-10-1994" },
+          { Name: "Country", Value: "Netherlands" },
+        ]);
+        this.logSent = true;
+      } catch (e) {
+        console.log("error", e);
+      }
+    },
+  },
+};
+</script>
